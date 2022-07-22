ebffc74408c092d1c04b443f0cf1e5e9f59fed3b
Shlok2 <81300534+Shlok2@users.noreply.github.com>
2022-07-22 13:40:35 +0530

    Add files via upload

---
+++ Linked_list/reverseOrder.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string.h>
+#include <list>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+    node *temp = head;
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+node *reverse(node *head)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+
+    while (currptr != NULL)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+    }
+    return prevptr;
+}
+
+int main(int argc, char const *argv[])
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    display(head);
+    node *newhead = reverse(head);
+    display(newhead);
+    return 0;
+}
